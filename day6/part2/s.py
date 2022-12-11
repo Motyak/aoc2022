5a8819273d2991169a63ac22f6901560cc8c96bd
Tommy Anfosso <anfossotommy@hotmail.fr>
2022-12-11 02:36:13 +0100

    class generator, algos need some rework

--- day6/part2/s.py
+++ day6/part2/s.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
-
 from collections.abc import Callable
 from typing import Generator
 
 def main():
     INPUT_FILENAME = "input.txt"
-
-
     dataStream = DataStream.fromTextFile(INPUT_FILENAME)
-    
-    startOfMessage = StartOfMessageMarker.fromDataStream(dataStream)
 
-    print(startOfMessage)
-
+    MARKER_SIZE = 14
+    StartOfMessageMarker = StartMarker(MARKER_SIZE)
+    marker = StartOfMessageMarker.fromDataStream(dataStream)
+    print(marker)
 
 class DataStream:
     @staticmethod
+    # yield function <=> generator <=> function retuning an iterator
     def fromTextFile(filename: str):
         assert isinstance(filename, str)
 
@@ -29,67 +27,66 @@ class DataStream:
 
 class StrategyGenerator:
     @staticmethod
+    # function generator
     def firstOccurenceOfNDifferentCharacters(n: int):
         assert isinstance(n, int)
-        def inner(currentData: str, nextChar: str):
-            assert isinstance(currentData, str)
+        assert n >= 0
+        
+        def strategy(buffer: str, nextChar: str):
+            assert isinstance(buffer, str)
             assert isinstance(nextChar, str)
             assert len(nextChar) == 1
 
-            CUR_DATA_MAX_SIZE = n - 1
+            BUFFER_MAX_SIZE = n - 1
             containsDuplicates = lambda x: len(x) != len(set(x))
-            return len(currentData) < CUR_DATA_MAX_SIZE \
-                or containsDuplicates(currentData[-CUR_DATA_MAX_SIZE:] + nextChar)
+            return len(buffer) < BUFFER_MAX_SIZE \
+                or containsDuplicates(buffer[-BUFFER_MAX_SIZE:] + nextChar)
         
-        return inner
+        return strategy
 
+# class generator
+def StartMarker(markerSize: int):
+    assert isinstance(markerSize, int)
+    assert markerSize >= 0
 
-# # return True as long as we need to continue reading
-# def firstOccurenceOfFourDifferentCharacters(currentData: str, nextChar: str):
-#     assert isinstance(currentData, str)
-#     assert isinstance(nextChar, str)
-#     assert len(nextChar) == 1
+    class StartMarkerImpl:
+        MARKER_SIZE: int = markerSize
+        STRATEGY: Callable = StrategyGenerator.firstOccurenceOfNDifferentCharacters(markerSize)
 
-#     def containsDuplicates(characters):
-#         return len(characters) != len(set(characters))
+        def __init__(self, marker: str, position: int):
+            assert isinstance(marker, str)
+            assert len(marker) == StartMarkerImpl.MARKER_SIZE
+            assert isinstance(position, int)
+            assert position >= 0
 
-#     return len(currentData) < 3 or containsDuplicates(currentData[-3:] + nextChar)
+            self.marker = marker
+            self.position = position
 
+        def __str__(self):
+            return f"{self.marker=}\t{self.position=}"
 
-class StartOfMessageMarker:
-    MARKER_SIZE = 14
-    STRATEGY = StrategyGenerator.firstOccurenceOfNDifferentCharacters(MARKER_SIZE)
+        @staticmethod
+        def fromDataStream(dataStream: Generator):
+            assert isinstance(dataStream, Generator)
 
-    def __init__(self, marker: str, position: int):
-        assert isinstance(marker, str)
-        assert len(marker) == StartOfMessageMarker.MARKER_SIZE
-        assert isinstance(position, int)
+            MARKER_SIZE = StartMarkerImpl.MARKER_SIZE
+            BUFFER_MAX_SIZE = StartMarkerImpl.MARKER_SIZE
+            STRATEGY = StartMarkerImpl.STRATEGY
 
-        self.marker = marker
-        self.position = position
-
-    def __str__(self):
-        return f"{self.marker=}\t{self.position=}"
-
-    @staticmethod
-    def fromDataStream(dataStream: Generator):
-        assert isinstance(dataStream, Generator)
-
-        MARKER_SIZE = StartOfMessageMarker.MARKER_SIZE
-        BUFFER_MAX_SIZE = MARKER_SIZE
-        STRATEGY = StartOfMessageMarker.STRATEGY
-
-        buffer = ""
-        position = 0
-        nextChar = next(dataStream)
-        while nextChar and STRATEGY(buffer, nextChar):
-            buffer = buffer[-BUFFER_MAX_SIZE:] + nextChar
+            buffer = ""
+            position = 0
+            nextChar = next(dataStream, None)
+            while nextChar and STRATEGY(buffer, nextChar):
+                buffer = buffer[-BUFFER_MAX_SIZE:] + nextChar
+                position += 1
+                nextChar = next(dataStream)
             position += 1
-            nextChar = next(dataStream)
-        position += 1
 
-        # marker has a size of four characters
-        marker = (buffer + nextChar)[-MARKER_SIZE:]
-        return StartOfMessageMarker(marker, position)
+            marker = (buffer + nextChar)[-MARKER_SIZE:]
+
+            return StartMarkerImpl(marker, position)
+    # -- end of class StartMarkerImpl
+
+    return StartMarkerImpl
 
 main() if __name__ == "__main__" else None
